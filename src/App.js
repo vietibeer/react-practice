70744ede0237a2112e08bee60ce313217ea36303
vietdoan <vietibeer@gmail.com>
2019-07-01 11:05:14 +0700

    make react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import './App.css';
 import Game from './game';
-import Caculator from './convert-temperature';
+import CaculatorTemperature from './convert-temperature';
+import Todos from './trainning-redux/todos-with-undo';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <Game />
-      </div>
+      <Router>
+        <div className="App">
+          <ul>
+            <li>
+              <Link to="/tik-tok">Game Tik-Tok</Link>
+            </li>
+            <li>
+              <Link to="/todos-with-undo">Todos with (Undo/Redo)</Link>
+            </li>
+            <li>
+              <Link to="/temperature">Convert Temperature</Link>
+            </li>
+          </ul>
+
+          <hr/>
+
+          <Route exact path="/tik-tok" component={Game} />
+          {/* <Route path="/todos-with-undo" component={Todos} /> */}
+          <Route path="/todos-with-undo" render={() => <h1>Todos with Redux</h1>} />
+          <Route path="/temperature" render={CaculatorTemperature} />
+        </div>
+      </Router>
     );
   }
 
---
+++ src/trainning-redux/todos-with-undo.js
@@ -0,0 +1,15 @@
+var React = require('react');
+
+class Todos extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    Todos redux
+                </h1>
+            </div>
+        );
+    }
+}
+
+export default Todos;
